ad42644b8d0c23af3d102d9622af2cd12252275d
José Ignacio Orlando <ignaciorlando@gmail.com>
2017-07-13 12:46:53 -0300

    Useless code removed

--- experiments/script_perform_correlation_analysis.m
+++ experiments/script_perform_correlation_analysis.m
@@ -20,12 +20,6 @@ switch extract_from
     case 'skeleton' 
         % prepare input tag
         input_tag = 'from-skeleton';    
-    case 'image'
-        % prepare input tag
-        input_tag = 'from-image';
-    case 'inpainted'
-        % prepare input tag
-        input_tag = 'from-inpainted'; 
 end
 
 %% 
@@ -42,13 +36,3 @@ end
 
 % now, compute correlation
 [correlation_matrix, p_values] = corr(feature_array)
-
-% also compute mutual information
-mutual_information_matrix = zeros(length(fractal_dimensions));
-for i = 1 : length(fractal_dimensions)
-    for j = i : length(fractal_dimensions)
-        mutual_information_matrix(i,j)=mutual_information(feature_array(:,i)',feature_array(:,j)');
-        mutual_information_matrix(i,j)=mutual_information_matrix(j,i);
-    end
-end
-mutual_information_matrix
